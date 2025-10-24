0541d1e8884a38eb1ec8414d05fb552465a7c4be
Felca <felicia.rfs.plg@gmail.com>
2025-10-24 22:41:15 +0700

    login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,10 +4,10 @@ import { Link, useNavigate } from "react-router-dom";
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
   const handleLogin = (e: React.FormEvent) => {
     e.preventDefault();
-    const navigate = useNavigate();
 
     const user = { email, password };
 
